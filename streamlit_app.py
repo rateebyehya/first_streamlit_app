a5057b941f25273515299b63055b34c37e8390d1
rateebyehya <107772014+rateebyehya@users.noreply.github.com>
2023-03-13 13:25:59 -0700

    Update streamlit_app.py
    user input

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,10 +28,11 @@ streamlit.header('Fruityvice Fruit Advice')
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
-#take the json version of the response and normalize it 
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-#output it to the screen as a table 
-streamlit.dataframe(fruityvice_normalized)
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+import requests
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit + fruit_choice)
 
 
 
